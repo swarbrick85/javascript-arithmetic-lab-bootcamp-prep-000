791e34250cdedbffd90a2662a72ddb11ea8ccb30
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-06-08 17:20:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ parseInt(n, 10)
 function add(a, b) {
   return a + b
 }
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
